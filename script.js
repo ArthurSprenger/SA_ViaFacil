8480918e53857a44fb5c257ba28564dd045aa3b1
FelipeCarvalhoLuz <felipe_carvalho-luz@estudante.sesisenai.org.br>
2025-04-01 09:50:59 -0300

    ADD

---
+++ script.js
@@ -0,0 +1,9 @@
+const spinner = document.querySelector('.spinner');
+
+let angle = 0; 
+
+function rotateSpinner() {
+  angle += 5; 
+  spinner.style.transform = `rotate(${angle}deg)`;
+}
+setInterval(rotateSpinner, 50);
