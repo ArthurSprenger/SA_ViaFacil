a9a40c0f49dc45b01c0d36c70cd59af304f4376d
FelipeCarvalhoLuz <felipe_carvalho-luz@estudante.sesisenai.org.br>
2025-11-06 12:04:13 -0300

    add

--- includes/phpMQTT.php
+++ includes/phpMQTT.php
@@ -192,3 +192,269 @@ class phpMQTT
         return true;
     }
 
+    public function read($int = 8192, $nb = false)
+    {
+        $string = '';
+        $togo = $int;
+
+        if ($nb) {
+            return fread($this->socket, $togo);
+        }
+
+        while (!feof($this->socket) && $togo > 0) {
+            $fread = fread($this->socket, $togo);
+            $string .= $fread;
+            $togo = $int - strlen($string);
+        }
+
+        return $string;
+    }
+
+    public function subscribe($topics, $qos = 0): void
+    {
+        $i = 0;
+        $buffer = '';
+        $id = $this->msgid;
+        $buffer .= chr($id >> 8);
+        $i++;
+        $buffer .= chr($id % 256);
+        $i++;
+
+        foreach ($topics as $key => $topic) {
+            $buffer .= $this->strwritestring($key, $i);
+            $buffer .= chr($topic['qos']);
+            $i++;
+            $this->topics[$key] = $topic;
+        }
+
+        $cmd = 0x82;
+        $cmd += ($qos << 1);
+
+        $head = chr($cmd);
+        $head .= $this->setmsglength($i);
+        fwrite($this->socket, $head, strlen($head));
+
+        $this->_fwrite($buffer);
+        $string = $this->read(2);
+
+        $bytes = ord(substr($string, 1, 1));
+        $this->read($bytes);
+    }
+
+    public function ping(): void
+    {
+        $head = chr(0xc0);
+        $head .= chr(0x00);
+        fwrite($this->socket, $head, 2);
+        $this->timesinceping = time();
+        $this->_debugMessage('ping sent');
+    }
+
+    public function disconnect(): void
+    {
+        $head = ' ';
+        $head[0] = chr(0xe0);
+        $head[1] = chr(0x00);
+        fwrite($this->socket, $head, 2);
+    }
+
+    public function close(): void
+    {
+        $this->disconnect();
+        stream_socket_shutdown($this->socket, STREAM_SHUT_WR);
+    }
+
+    public function publish($topic, $content, $qos = 0, $retain = false): void
+    {
+        $i = 0;
+        $buffer = '';
+
+        $buffer .= $this->strwritestring($topic, $i);
+
+        if ($qos) {
+            $id = $this->msgid++;
+            $buffer .= chr($id >> 8);
+            $i++;
+            $buffer .= chr($id % 256);
+            $i++;
+        }
+
+        $buffer .= $content;
+        $i += strlen($content);
+
+        $head = ' ';
+        $cmd = 0x30;
+        if ($qos) {
+            $cmd += $qos << 1;
+        }
+        if (empty($retain) === false) {
+            ++$cmd;
+        }
+
+        $head[0] = chr($cmd);
+        $head .= $this->setmsglength($i);
+
+        fwrite($this->socket, $head, strlen($head));
+        $this->_fwrite($buffer);
+    }
+
+    protected function _fwrite($buffer)
+    {
+        $buffer_length = strlen($buffer);
+        for ($written = 0; $written < $buffer_length; $written += $fwrite) {
+            $fwrite = fwrite($this->socket, substr($buffer, $written));
+            if ($fwrite === false) {
+                return false;
+            }
+        }
+        return $buffer_length;
+    }
+
+    public function message($msg)
+    {
+        $tlen = (ord($msg[0]) << 8) + ord($msg[1]);
+        $topic = substr($msg, 2, $tlen);
+        $msg = substr($msg, ($tlen + 2));
+        $found = false;
+        foreach ($this->topics as $key => $top) {
+            if (preg_match(
+                '/^' . str_replace(
+                    '#',
+                    '.*',
+                    str_replace(
+                        '+',
+                        "[^\/]*",
+                        str_replace(
+                            '/',
+                            "\/",
+                            str_replace(
+                                '$',
+                                '\$',
+                                $key
+                            )
+                        )
+                    )
+                ) . '$/',
+                $topic
+            )) {
+                $found = true;
+
+                if (is_callable($top['function'])) {
+                    call_user_func($top['function'], $topic, $msg);
+                }
+            }
+        }
+
+        if ($found === false) {
+            $this->_debugMessage('msg received but no match in subscriptions');
+        }
+
+        return $found;
+    }
+
+    public function proc(bool $loop = true)
+    {
+        if (feof($this->socket)) {
+            $this->_debugMessage('eof receive going to reconnect for good measure');
+            fclose($this->socket);
+            $this->connect_auto(false);
+            if (count($this->topics)) {
+                $this->subscribe($this->topics);
+            }
+        }
+
+        $byte = $this->read(1, true);
+
+        if ((string)$byte === '') {
+            if ($loop === true) {
+                usleep(100000);
+            }
+        } else {
+            $cmd = (int)(ord($byte) / 16);
+            $this->_debugMessage(
+                sprintf(
+                    'Received CMD: %d (%s)',
+                    $cmd,
+                    isset(static::$known_commands[$cmd]) === true ? static::$known_commands[$cmd] : 'Unknown'
+                )
+            );
+
+            $multiplier = 1;
+            $value = 0;
+            do {
+                $digit = ord($this->read(1));
+                $value += ($digit & 127) * $multiplier;
+                $multiplier *= 128;
+            } while (($digit & 128) !== 0);
+
+            $this->_debugMessage('Fetching: ' . $value . ' bytes');
+
+            $string = $value > 0 ? $this->read($value) : '';
+
+            if ($cmd) {
+                switch ($cmd) {
+                    case 3:
+                        $return = $this->message($string);
+                        if (is_bool($return) === false) {
+                            return $return;
+                        }
+                        break;
+                }
+            }
+        }
+
+        if ($this->timesinceping < (time() - $this->keepalive)) {
+            $this->_debugMessage('not had something in a while so ping');
+            $this->ping();
+        }
+
+        if ($this->timesinceping < (time() - ($this->keepalive * 2))) {
+            $this->_debugMessage('not seen a packet in a while, disconnecting/reconnecting');
+            fclose($this->socket);
+            $this->connect_auto(false);
+            if (count($this->topics)) {
+                $this->subscribe($this->topics);
+            }
+        }
+
+        return true;
+    }
+
+    protected function setmsglength($len): string
+    {
+        $string = '';
+        do {
+            $digit = $len % 128;
+            $len >>= 7;
+            if ($len > 0) {
+                $digit |= 0x80;
+            }
+            $string .= chr($digit);
+        } while ($len > 0);
+        return $string;
+    }
+
+    protected function strwritestring($str, &$i): string
+    {
+        $len = strlen($str);
+        $msb = $len >> 8;
+        $lsb = $len % 256;
+        $ret = chr($msb);
+        $ret .= chr($lsb);
+        $ret .= $str;
+        $i += ($len + 2);
+        return $ret;
+    }
+
+    protected function _debugMessage(string $message): void
+    {
+        if ($this->debug === true) {
+            echo date('Y-m-d H:i:s') . ' - ' . $message . PHP_EOL;
+        }
+    }
+
+    protected function _errorMessage(string $message): void
+    {
+        error_log('MQTT Error: ' . $message);
+    }
+}
